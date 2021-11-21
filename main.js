182775cfdf355e8eab3be289fb8e6af4a6637ef6
jlopcun <62543437+jlopcun@users.noreply.github.com>
2021-11-21 14:14:19 +0100

    adding some products to the page

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 const $id = (element) => document.getElementById(element);
 
+const $body = document.body;
 const $navBar__ul = $id('navBar__ul');
 const $navBar__button = $id('navBar__button');
-
-
+const $darkLight = $id('darkLight');
+const $pageContent = $id('pageContent');
+const RootStyles = document.documentElement.style;
 $navBar__button.addEventListener('click',()=>{
     $navBar__button.classList.toggle('opened');
     $navBar__ul.classList.toggle('opened');
+    $body.classList.toggle('menuOpened');
+    $pageContent.classList.toggle('menuOpened');
 })
+
+$darkLight.addEventListener('click',(e)=>{
+    if(e.target.src.includes('sun')){
+        RootStyles.setProperty('--pageMainColor','#777');
+        RootStyles.setProperty('--prouctBgcolor','#aaa');
+        e.target.src="assets-microondi/moon-svgrepo-com.svg";
+    }
+    else{
+        RootStyles.setProperty('--pageMainColor','#fff');
+        RootStyles.setProperty('--prouctBgcolor','#eee');
+        e.target.src="assets-microondi/sun-svgrepo-com.svg";
+    }
+})
+
+
+
