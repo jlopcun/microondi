7ff40c341aa74b92ce181ca78fb11c8c4ad8359f
jlopcun <62543437+jlopcun@users.noreply.github.com>
2022-02-26 10:55:41 +0100

    improving the page design

--- main.js
+++ main.js
@@ -13,28 +13,28 @@ $navBar__button.addEventListener('click',()=>{
     $body.classList.toggle('menuOpened');
     $pageContent.classList.toggle('menuOpened');
 })
-const ThemeLight = ()=>{
-    RootStyles.setProperty('--pageMainColor','#fff');
-    (window.location.toString().includes('semi-pages'))
-    ?$darkLight.src="../assets-microondi/sun-svgrepo-com.svg"
-    :$darkLight.src="assets-microondi/sun-svgrepo-com.svg"
-    localStorage.setItem('mode','sun');
-}
-const ThemeDark =()=>{
-    RootStyles.setProperty('--pageMainColor','#777');
-        (window.location.toString().includes('semi-pages'))
-        ?$darkLight.src="../assets-microondi/moon-svgrepo-com.svg"
-        :$darkLight.src="assets-microondi/moon-svgrepo-com.svg"
-        localStorage.setItem('mode','moon');
-}
-$darkLight.addEventListener('click',(e)=>{
-    if(e.target.src.includes('sun')){
-       ThemeDark(e);
-    }
-    else{
-       ThemeLight(e);
-    }
-})
+// const ThemeLight = ()=>{
+//     RootStyles.setProperty('--pageMainColor','#fff');
+//     (window.location.toString().includes('semi-pages'))
+//     ?$darkLight.src="../assets-microondi/sun-svgrepo-com.svg"
+//     :$darkLight.src="assets-microondi/sun-svgrepo-com.svg"
+//     localStorage.setItem('mode','sun');
+// }
+// const ThemeDark =()=>{
+//     RootStyles.setProperty('--pageMainColor','#777');
+//         (window.location.toString().includes('semi-pages'))
+//         ?$darkLight.src="../assets-microondi/moon-svgrepo-com.svg"
+//         :$darkLight.src="assets-microondi/moon-svgrepo-com.svg"
+//         localStorage.setItem('mode','moon');
+// }
+// $darkLight.addEventListener('click',(e)=>{
+//     if(e.target.src.includes('sun')){
+//        ThemeDark(e);
+//     }
+//     else{
+//        ThemeLight(e);
+//     }
+// })
 
 
 
