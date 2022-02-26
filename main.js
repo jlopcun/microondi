9c6e91dca9807012b4b2cf3395fd45c81f2cec50
jlopcun <62543437+jlopcun@users.noreply.github.com>
2022-02-26 14:04:14 +0100

    adding the carrousel

--- main.js
+++ main.js
@@ -5,14 +5,141 @@ const $navBar__ul = $id('navBar__ul');
 const $navBar__button = $id('navBar__button');
 const $darkLight = $id('darkLight');
 const $pageContent = $id('pageContent');
-const $loader = $id('loader');
+const $productsContainer = $id('productsContainer');
 const RootStyles = document.documentElement.style;
+
+
 $navBar__button.addEventListener('click',()=>{
     $navBar__button.classList.toggle('opened');
     $navBar__ul.classList.toggle('opened');
     $body.classList.toggle('menuOpened');
     $pageContent.classList.toggle('menuOpened');
 })
+
+
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+    const saw = (entries) =>{
+        entries.forEach(el=>{
+            if(el.isIntersecting) console.log(el)
+        })
+    }
+    const observer = new IntersectionObserver(saw,{threshold:.50});
+    Array.from($productsContainer.children).forEach(el=>{
+        observer.observe(el)
+    })
+})
+
+
+
+
+
+const $Carrouselimg = $id('Carrouselimg');
+const $Carrouselmarker = $id('Carrouselmarker');
+let CVcarrousel = 1;
+$Carrouselmarker.children[CVcarrousel-1].classList.add('showing');
+setInterval(()=>{
+    (CVcarrousel===4)?CVcarrousel=1:CVcarrousel+=1;
+    $Carrouselimg.src=`assets-microondi/flyers/microondi-flyer-${CVcarrousel}.jpg`;
+    Array.from($Carrouselmarker.children).forEach(el=>{
+        el.classList.remove('showing');
+    })
+    $Carrouselmarker.children[CVcarrousel-1].classList.add('showing');
+},9000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const ThemeLight = ()=>{
 //     RootStyles.setProperty('--pageMainColor','#fff');
 //     (window.location.toString().includes('semi-pages'))
@@ -38,14 +165,11 @@ $navBar__button.addEventListener('click',()=>{
 
 
 
-window.addEventListener('load',()=>{
-    if(localStorage.getItem('mode')==='sun'){
-        ThemeLight();
-    }
-    else{
-        ThemeDark();
-    }
-})
-setTimeout(()=>{
-    $loader.classList.add('loader-load');
-},1000)
+// window.addEventListener('load',()=>{
+//     if(localStorage.getItem('mode')==='sun'){
+//         ThemeLight();
+//     }
+//     else{
+//         ThemeDark();
+//     }
+// })
